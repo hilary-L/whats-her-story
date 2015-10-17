3b616dd0f3b3a8546dda9b360ba0d2778a62ba17
Hilary Lee <hilary@digintent.com>
2015-10-17 09:13:46 -0500

    Update boilerplate, fix package.json.

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -3,10 +3,43 @@ import React from 'react';
 
 var Main = React.createClass({
 
-	render: function() {
+	getInitialState: function() {
 
 		return (
-			<div>HI!!!</div>
+				{
+					storyProperties: {
+
+
+					},
+					storyDisplay {
+
+					},
+					codeDisplay {
+
+					}
+
+				}
+		)
+
+	},
+
+
+	render: function() {
+
+		// Main is top level component where inputs will be, has state. //
+		// Two child components, story view and code view. Will be siblings. //
+		// Use object dictionary for inputs, handleGetInput: function() {}, var inputObject = {'prop1', 'prop2' }  //
+		// inputObject[nameSentFromInputBind] = the contents of the input //
+
+		}
+
+		return (
+			<div className="container">
+				<StoryBoard />
+				<div className="view-container">
+					<CodeBoard />
+				</div>
+			</div>
 
 		)
 	}
