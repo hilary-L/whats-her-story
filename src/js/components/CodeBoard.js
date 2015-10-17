6e229131e1c202e09a9562669732e781afda9be7
Sam Goodman <superimposition@gmail.com>
2015-10-17 13:23:40 -0500

    Adding code snippets; removing app.css from repo

--- src/js/components/CodeBoard.js
+++ src/js/components/CodeBoard.js
@@ -6,10 +6,19 @@ var CodeBoard = React.createClass({
 
 		return (
 			<div className="card">
-				<p className={this.props.codeDisplay.firstBlock ? 'codeblock' : 'codeblock hidden'}>
+				<pre className={this.props.codeDisplay.firstBlock ? 'codeblock' : 'codeblock hidden'}>
 					<p>{'var characterName = '} {this.props.storyProperties.name}</p>
 					<p>{'var lookingFor = '} {this.props.storyProperties.lookingFor}</p>
-				</p>
+				</pre>
+				<pre className={this.props.codeDisplay.secondBlock ? 'codeblock' : 'codeblock hidden'}>
+					<p>{'var leftRoom = '} {this.props.storyProperties.leftRoom}</p>
+					<p>{'var rightRoom = '} {this.props.storyProperties.rightRoom}</p>
+				</pre>
+				<pre className={this.props.codeDisplay.thirdBlock ? 'codeblock' : 'codeblock hidden'}>
+					<p>{'var obstacle = '} {this.props.storyProperties.obstacle}</p>
+					<p>{'var treasure = '} {this.props.storyProperties.treasure}</p>
+					<p>{'var cave = '} {this.props.storyProperties.cave}</p>
+				</pre>
 			</div>
 
 		)
