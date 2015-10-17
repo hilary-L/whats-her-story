699192e3a9963884e5c337d9aea5bdec83d2ce2a
Hilary Lee <hilary@digintent.com>
2015-10-17 12:03:36 -0500

    Update page layout.

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -18,6 +18,12 @@ var Main = React.createClass({
 					envelope: '',
 					present: '',
 				},
+				page: {
+					pageOne: true,
+					pageTwo: false,
+					pageThree: false,
+					pageFour: false
+				},
 				storyDisplay: {},
 				codeDisplay: {
 					firstBlock: false,
@@ -31,17 +37,27 @@ var Main = React.createClass({
 
 	},
 
+	handleChangePage: function(page) {
+
+		console.log(page);
+
+		var page = {
+			pageOne: false,
+			pageTwo: false,
+			pageThree: false,
+			pageFour: false
+		}
+
+		page[page] = true;
+
+		this.setState({
+			page: page
+		});
+	},
+
 	handleInput: function(property, block, event) {
 		
-		var storyProperties = {
-			name: '',
-			lookingFor: '',
-			leftRoom: '',
-			rightRoom: '',
-			treasure: '',
-			envelope: '',
-			present: ''
-		}
+		var storyProperties = this.state.storyProperties;
 
 		var codeDisplay = {
 			firstBlock: false,
@@ -76,10 +92,16 @@ var Main = React.createClass({
 			<div className="container">
 				<StoryBoard />
 				<div className="view-container">
-					<div className="card">
-						<p>Test paragraph</p>
-						<input type="text" value={this.state.storyProperties.name} onChange={this.handleInput.bind(null, 'name', 'firstBlock')}></input>
-						<a href="#" className="btn btn-primary">Button</a>	
+					<div className={this.state.page.pageOne ? 'card' : 'hidden card'}>
+						<p>What is your character name? <input type="text" value={this.state.storyProperties.name} onChange={this.handleInput.bind(null, 'name', 'firstBlock')}></input></p>
+						<p>What does your character seek? <input type="text" value={this.state.storyProperties.lookingFor} onChange={this.handleInput.bind(null, 'lookingFor', 'firstBlock')}></input></p>
+						<a onClick={this.handleChangePage.bind(null, 'pageTwo')} className="btn btn-primary">Next</a>	
+					</div>
+					<div className={this.state.page.pageTwo ? 'card' : 'hidden card'}>
+						<p>There is a door to your left and a door to your right.</p>
+						<p>What is the setting behind the door to your left? <input type="text" value={this.state.storyProperties.leftRoom} onChange={this.handleInput.bind(null, 'leftRoom', 'secondBlock')}></input></p>
+						<p>What is the setting behind the door to your right? <input type="text" value={this.state.storyProperties.rightRoom} onChange={this.handleInput.bind(null, 'rightRoom', 'secondBlock')}></input></p>
+						<a onClick={this.handleChangePage.bind(null, 'pageThree')} className="btn btn-primary">Next</a>	
 					</div>
 					<CodeBoard codeDisplay={this.state.codeDisplay} storyProperties={this.state.storyProperties} />
 				</div>
