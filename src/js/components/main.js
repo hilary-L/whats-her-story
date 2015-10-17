3988974680ddba77e594a1624b7667242d4b2d88
Hilary Lee <hilary@digintent.com>
2015-10-16 21:26:50 -0500

    Include react in boilerplate files, finalize script locations.

---
+++ src/js/components/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+var Main = React.createClass({
+
+	render: function() {
+
+		return (
+			<div>HI!!!</div>
+
+		)
+	}
+});
+
+
+React.render(<Main />, document.getElementById('app'));
