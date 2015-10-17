5d5ff07827bbd53d46ba8bb6cd7c94f7df149208
Patrick Bakke <patrickdbakke@gmail.com>
2015-10-17 14:59:20 -0500

    fixing pages

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -238,17 +238,6 @@ var Main = React.createClass({
 							<a onClick={this.handleChangePage.bind(null, 'pageThree')} className="btn btn-primary">Next</a>	
 						</div>
 						<div className={this.state.inputPage.pageThree ? 'card' : 'hidden card'}>
-							<p>Okay, let us go back to the door on the right.  Behind that door, {this.state.storyProperties.rightRoom}, there are two people, one mean and one nice.</p>
-							<p>Who is the mean person? <input type="text" value={this.state.storyProperties.meanPerson} onChange={this.handleInput.bind(null, 'meanPerson', 'thirdBlock')}></input></p>
-							<p>Who is the nice person? <input type="text" value={this.state.storyProperties.nicePerson} onChange={this.handleInput.bind(null, 'nicePerson', 'thirdBlock')}></input></p>
-							<a onClick={this.handleChangePage.bind(null, 'pageFour')} className="btn btn-primary">Next</a>	
-						</div>
-						<div className={this.state.inputPage.pageFour ? 'card' : 'hidden card'}>
-							<p>If you talk to the mean person, they hand you an envelope.  You suspect it is full of poison, but just to be sure you open it.  What is inside?  <input type="text" value={this.state.storyProperties.envelope} onChange={this.handleInput.bind(null, 'envelope', 'fourthBlock')}></input></p>
-							<p>If you talk to the nice person, they are delighted to see you!  They give you a present.  What is in the present?  <input type="text" value={this.state.storyProperties.present} onChange={this.handleInput.bind(null, 'present', 'fourthBlock')}></input></p>
-							<a onClick={this.handleChangePage.bind(null, 'pageFive')} className="btn btn-primary">Next</a>	
-						</div>
-						<div className={this.state.inputPage.pageThree ? 'card' : 'hidden card'}>
 							<p>Behind the door to your left, the {this.state.storyProperties.leftRoom}, there is an obstacle!  What is it? <input type="text" value={this.state.storyProperties.obstacle} onChange={this.handleInput.bind(null, 'obstacle', 'thirdBlock')}></input></p>
 							<p>If your character, {this.state.storyProperties.name}, overcomes the obstacle, they are magically transported to a castle.  In the castle is a treasure chest.  But what is in the treasure chest? <input type="text" value={this.state.storyProperties.treasure} onChange={this.handleInput.bind(null, 'treasure', 'thirdBlock')}></input></p>
 							<p>If your character does not overcome the obstacle, they are transported to the bottom of the sea.  It is not very fun down there, but there is a glowing cave.  What is in the cave? <input type="text" value={this.state.storyProperties.cave} onChange={this.handleInput.bind(null, 'cave', 'thirdBlock')}></input></p>
